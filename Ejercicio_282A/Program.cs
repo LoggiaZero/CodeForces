88cb425de16791bfef41590afd4b1ef850bbbead
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-05-29 00:13:29 -0600

    Ejercicio_282A

---
+++ Ejercicio_282A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicio_282A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            for(int n = int.Parse(Console.ReadLine());n>0;--n)
+            {
+                x += Console.ReadLine().Contains("+") ? 1 : -1;
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
