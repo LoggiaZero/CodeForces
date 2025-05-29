9b5118630ceabb586936a51c4f31884b2fd8fe9c
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-05-29 00:30:27 -0600

    Ejercicio_1A

---
+++ Ejercicio_1A/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejercicio_1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] data = Array.ConvertAll<string, double>(Console.ReadLine().Split(' '), double.Parse);
+            Console.WriteLine((long)((Math.Ceiling(data[0] / data[2]) * Math.Ceiling(data[1]/data[2]))));
+        }
+    }
+}
