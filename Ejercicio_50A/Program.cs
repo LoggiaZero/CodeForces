add8aece746d0ad590429d6b530c578c3c07e263
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-05-29 00:58:52 -0600

    Ejercicio_50A

---
+++ Ejercicio_50A/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejercicio_50A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int m = int.Parse(data[0]);
+            int n = int.Parse(data[1]);
+            Console.WriteLine((m * n) / 2);
+        }
+    }
+}
