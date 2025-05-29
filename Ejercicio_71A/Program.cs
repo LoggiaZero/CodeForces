b521954121001749ffa62f38e696e20016f16b0d
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-05-28 23:29:31 -0600

    Ejercicio 71A

---
+++ Ejercicio_71A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Ejercicio_71A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder answer = new StringBuilder();
+            string word;
+            int count;
+            for(int n = int.Parse(Console.ReadLine());n>0;--n)
+            {
+                word = Console.ReadLine();
+                count = word.Length;
+                if (count > 10)
+                    answer.AppendLine($"{word[0]}{count - 2}{word[count - 1]}");
+                else
+                    answer.AppendLine(word);
+            }
+            Console.Write(answer);
+        }
+    }
+}
