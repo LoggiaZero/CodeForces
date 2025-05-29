ae60afab94f5fed43de0e8dbd5bfca0265d2615d
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-05-29 01:10:23 -0600

    Ejercicio 263A

---
+++ Ejercicio_263A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicio_263A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int i, j;
+            for(i=0; !(line=Console.ReadLine()).Contains("1");++i) ;
+            string[] data = line.Split(' ');
+            for (j=0; data[j] != "1"; ++j) ;
+            Console.WriteLine(Math.Abs(2 - i) + Math.Abs(2 - j));
+        }
+    }
+}
