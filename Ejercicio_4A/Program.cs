fb5ae515b618afdaee9cbed47799e1dac0cb4db6
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-05-28 23:18:23 -0600

    Ejericio 4A

---
+++ Ejercicio_4A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio_4A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int weight = int.Parse(Console.ReadLine());
+            if (weight > 2 && (weight & 1) == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
