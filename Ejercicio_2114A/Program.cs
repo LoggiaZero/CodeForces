9dde8bb6efd736f98cca1de7794828f9a46cd9a2
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-05-28 22:44:11 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio_2114A/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ejercicio_2114A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            int minyear;
+            int a, b;
+            for(int tc = int.Parse(Console.ReadLine());tc>0;--tc)
+            {
+                year = int.Parse(Console.ReadLine());
+                minyear = (int)Math.Sqrt(year);
+                if(minyear*minyear == year)
+                {
+                    if((minyear & 1) == 0)
+                    {
+                        a = minyear / 2;
+                        b = a;
+                    }
+                    else
+                    {
+                        a = minyear / 2;
+                        b = a + 1;
+                    }
+                    Console.WriteLine($"{a} {b}");
+                }
+                else
+                {
+                    Console.WriteLine(-1);
+                }
+            }
+        }
+    }
+}
