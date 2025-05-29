986f5c066bae6027eb70a626cc08ec74080303e8
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-05-28 23:51:31 -0600

    Ejercicio_581A

---
+++ Ejericio_581A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejericio_581A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] socks = Array.ConvertAll<string, int>(Console.ReadLine().Split(' '), int.Parse);
+            int firstwear = Math.Min(socks[0], socks[1]);
+            socks[0] -= firstwear;
+            socks[1] -= firstwear;
+            Console.Write($"{firstwear} {socks[0]/2 + socks[1]/2}");
+        }
+    }
+}
