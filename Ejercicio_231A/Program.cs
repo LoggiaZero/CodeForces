918ad9182955f3e0a0707b487b37584819097045
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-05-29 00:02:07 -0600

    Ejercicio_231A

---
+++ Ejercicio_231A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Ejercicio_231A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> values;
+            int votes;
+            int implementations = 0;
+            for(int n = int.Parse(Console.ReadLine());n>0;--n)
+            {
+                values = Array.ConvertAll<string, int>(Console.ReadLine().Split(' '), int.Parse).ToList();
+                votes = values.Sum();
+                implementations += votes >= 2 ? 1 : 0;
+            }
+            Console.Write(implementations);
+        }
+    }
+}
