b8b15d703ca15eca5ee95d8bc6e058307b802068
jase.1588@gmail.com <jase1588@gmail.com>
2025-05-29 20:07:45 -0600

    Ejercicio 2110B

---
+++ Ejercicio_2110B/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Ejercicio_2110B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool balanced;
+            StringReader brackets;
+            int openbrackets=0;
+            char bracket;
+            for (int n = int.Parse(Console.ReadLine()); n > 0; --n)
+            {
+                balanced = false;
+                brackets = new StringReader(Console.ReadLine());
+                for (bracket = (char)brackets.Read(); !balanced; bracket = (char)brackets.Read())
+                {
+                    openbrackets += bracket == '(' ? 1 : -1;
+                    balanced = openbrackets == 0;
+                }
+                
+                Console.WriteLine(brackets.Read()==-1?"NO":"YES");
+            }
+        }
+    }
+}
