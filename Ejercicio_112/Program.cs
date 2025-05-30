25d752180973ba8d83ffb2ce355fde67fdc028b4
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-05-30 01:14:57 -0600

    Ejercicio 112

---
+++ Ejercicio_112/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+namespace Ejercicio_112
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            BigInteger c;
+            string[] line = Console.ReadLine().Split(' ');
+            a = int.Parse(line[0]);
+            b = int.Parse(line[1]);
+            if (a == b)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                c = BigInteger.Pow((BigInteger)a, b) - BigInteger.Pow((BigInteger)b, a);
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
