8a863fb729fab8d606a76f8b2710e274e0312a88
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-05-30 00:07:07 -0600

    Ejercicio 115

---
+++ Ejercicio_115/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio_115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] daymonth = Array.ConvertAll<string, int>(Console.ReadLine().Split(' '), int.Parse);
+            DateTime start = new DateTime(2000, 12, 31);
+            start = start.AddDays(daymonth[0]).AddMonths(daymonth[1]-1);
+            if(start.Year == 2001 && start.Day == daymonth[0] && start.Month == daymonth[1])
+                Console.WriteLine(start.DayOfWeek == DayOfWeek.Sunday ? 7 : (int)start.DayOfWeek);
+            else
+                Console.WriteLine("Impossible");
+            Console.ReadLine();
+        }
+    }
+}
