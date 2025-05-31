a9807e280050facb4a5375e111f5ff6ec5a74c5f
jase.1588@gmail.com <jase1588@gmail.com>
2025-05-31 16:23:12 -0600

    Ejercicio_105

---
+++ Ejercicio_105/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio_105
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a = 0, b = 0, i;
+            for (i = 1; i != n; ++i)
+            {
+                a = (a + i) % 3;
+                b += a == 0 ? 1 : 0;
+            }
+            a = (a + i) % 3;
+            b += a == 0 ? 1 : 0;
+            Console.WriteLine(b);
+        }
+    }
+}
