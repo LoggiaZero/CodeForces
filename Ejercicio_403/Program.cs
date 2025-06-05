6c3b3b14a657b96bdff7f0828c4d6041b09599e9
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-06-04 22:49:31 -0600

    Ejercicio_403

---
+++ Ejercicio_403/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejercicio_403
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long num = long.Parse(Console.ReadLine());
+            Console.WriteLine($"{num*2+1}");
+        }
+    }
+}
