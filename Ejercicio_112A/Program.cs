ea50eb3708454bbf21e6293b1e878118796712da
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-06-04 22:21:40 -0600

    Ejercicio_112A

---
+++ Ejercicio_112A/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ejercicio_112A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            Console.WriteLine(string.Compare(a.ToLower(), b.ToLower()));
+        }
+    }
+}
